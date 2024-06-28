5afc90ef494be4762bf4ddef95ec500e6fdda4df
parahumanoid <57003748+parahumanoid@users.noreply.github.com>
2024-06-28 15:27:19 +0300

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,13 @@
+function filterArray(numbers, value) {
+    let result = new Array();
+    for (const currentValue of numbers) {
+        if (currentValue > value) result.push(currentValue);
+    }
+    return result;
+}
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+console.log("------------- end task 3 -------------");
